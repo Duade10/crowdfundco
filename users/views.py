3f75cb84f13ee44dcdabcfc56fa5418f551f1d36
Otunla Timilehin <69770115+Duade10@users.noreply.github.com>
2023-10-27 14:37:29 +0100

    added login and logout functions

--- users/views.py
+++ users/views.py
@@ -2,6 +2,9 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserRegistrationSerializer
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+from rest_framework.authentication import TokenAuthentication
 
 
 class UserRegistrationView(APIView):
@@ -12,21 +15,28 @@ class UserRegistrationView(APIView):
             user.login_method = "email"
             user.is_active = False
             user.verify_email(request)
-            serializer = UserRegistrationSerializer(user)
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-# from rest_framework.authtoken.models import Token
+class UserLoginView(APIView):
+    def post(self, request, format=None):
+        username = request.data.get("username")
+        password = request.data.get("password")
 
-# class UserRegistrationView(APIView):
-#     def post(self, request, format=None):
-#         serializer = UserRegistrationSerializer(data=request.data)
-#         if serializer.is_valid():
-#             user = serializer.save()
-#             user.is_active = False
-#             user.verify_email(request)
-#             # Create a token for the user
-#             token, created = Token.objects.get_or_create(user=user)
-#             return Response({'token': token.key}, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({"token": token.key}, status=status.HTTP_200_OK)
+        else:
+            return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class UserLogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+
+    def post(self, request, format=None):
+        request.auth.delete()
+        return Response({"message": "User logged out"}, status=status.HTTP_200_OK)
